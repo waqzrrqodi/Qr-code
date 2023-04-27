134cb3c2a0917409ff42fbb2d9ace91ac5d1497f
waqzrrqodi <kasper.hagg@skola.taby.se>
2023-04-27 14:00:16 +0200

    Best push v1.21.21.432

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const box = document.getElementById("colour-text");
-      const buttons = document.querySelectorAll(".colour-button");
+const buttons = document.querySelectorAll(".colour-button");
 
-      buttons.forEach(button => {
-        button.addEventListener("click", () => {
-          box.style.color = button.classList[1];
-        });
-      });
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    box.style.color = button.classList[1];
+    const choosenColour = button.classList[1];
+    console.log(choosenColour);});
+  });
 
 // Generate QR Code
 const qrCodeURL = document.querySelector('#qr-code');
@@ -14,4 +15,10 @@ const eyeChoice = document.querySelector('#eye-choice');
 const qrCodeClr = document.querySelector('#qr-code-clr');
 
 function generateQrCode() {
+  const choosenColour = document.querySelector('#colour-text').style.color;
+  const choosenEye = eyeChoice.options[eyeChoice.selectedIndex].value;
+  const choosenModule = moduleChoice.options[moduleChoice.selectedIndex].value;
+  event.preventDefault();
+  console.log('Generating QR Code...');
+  console.log(choosenColour);
 }
