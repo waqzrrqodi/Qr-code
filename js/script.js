c373c5fd1234ad06f1ec700708b1c67bbb3177e6
waqzrrqodi <kasper.hagg@skola.taby.se>
2023-05-09 23:15:50 +0200

    I'm guessing this may start causing us problems either soon or never.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { API_KEY } from './config.js';
+
 const box = document.getElementById("colour-text");
 const buttons = document.querySelectorAll(".colour-button");
 
@@ -14,11 +16,37 @@ const moduleChoice = document.querySelector('#module-choice');
 const eyeChoice = document.querySelector('#eye-choice');
 const qrCodeClr = document.querySelector('#qr-code-clr');
 
+
+const options = {
+  method: 'POST',
+  headers: {
+    Accept: 'image/svg+xml',
+    'Content-Type': 'application/json',
+    'X-RapidAPI-Key': API_KEY,
+  },
+  body: JSON.stringify({
+    data: 'https://koebra.tk',
+    style: {
+      module: {color: 'black', shape: 'default'},
+      inner_eye: {shape: 'default'},
+      outer_eye: {shape: 'default'},
+    },
+    size: {width: 200, quiet_zone: 4, error_correction: 'M'},
+    output: {filename: 'qrcode', format: 'svg'}
+  })
+};
+
 function generateQrCode() {
-  const choosenColour = document.querySelector('#colour-text').style.color;
-  const choosenEye = eyeChoice.options[eyeChoice.selectedIndex].value;
-  const choosenModule = moduleChoice.options[moduleChoice.selectedIndex].value;
-  event.preventDefault();
-  console.log('Generating QR Code...');
-  console.log(choosenColour);
+  fetch('https://qrcode3.p.rapidapi.com/qrcode/text', options)
+    .then(response => response)
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    console.log('generateQrCode() ran');
+    response.json().then(function(data) {
+      console.log(data);
+      document.getElementById("qr-code").innerHTML = data;
+    }
+    );
 }
+
+generateQrCode();
